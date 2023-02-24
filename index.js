bbd36722b2e4324440538f542ce17806557daa3f
Sylvester <sylvestertrevi@gmail.com>
2023-02-24 02:05:43 -0500

    pass tests

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+const currentUser = `Grace Hopper`; //string stored to a variable. 
+
+const welcomeMessage = `Welcome to Flatbook,  + ${currentUser}!`; //string w/ interpolation.
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
+
+
